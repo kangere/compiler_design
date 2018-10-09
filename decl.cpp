68652024d89a0005dc274abb7adda804d35a153a
kangere <kangere721@gmail.com>
2018-10-09 08:52:50 -0400

    added param_list to Func_decl class, refactored code

--- decl.cpp
+++ decl.cpp
@@ -1,7 +1,34 @@
 #include "decl.hpp"
 
+static void
+print_var(Var_decl* d)
+{
+
+}
+
+static void
+print_ref(Ref_decl* d)
+{
+
+}
+
+static void
+print_func(Func_decl* d)
+{
+
+}
+
 void
 print(Decl* d)
 {
+	switch(d->get_kind())
+	{
+		case Decl::var_decl:
+			return print_var(static_cast<Var_decl*>(d));
+		case Decl::ref_decl:
+			return print_ref(static_cast<Ref_decl*>(d));
+		case Decl::func_decl:
+			return print_func(static_cast<Func_decl*>(d));
+	}
 
 };
--- decl.hpp
+++ decl.hpp
@@ -4,25 +4,38 @@
 #include "type.hpp"
 #include "stmt.hpp"
 #include <string>
+#include <vector>
 
 class Expr;
 class Stmt;
 
+
 class Decl 
 {
+public:
+	enum kind
+	{
+		var_decl,
+		ref_decl,
+		func_decl
+	};
+
 private:
 	std::string m_name;
 	Type* m_type;
+	kind m_kind;
 
 protected:
-	Decl(std::string name, Type* t):
-	m_name(name), m_type(t)
+	Decl(std::string name, Type* t, kind k):
+	m_name(name), m_type(t), m_kind(k)
 	{}
 
 public:
 	Type* get_type() const { return m_type;}
 	std::string get_name() const { return m_name;}
+	kind get_kind() const {return m_kind;}
 
+	
 };
 
 
@@ -34,7 +47,7 @@ private:
 
 public:
 	Var_decl(std::string name, Type* t, Expr* e)
-	: Decl(name,t), m_exp(e) 
+	: Decl(name,t,var_decl), m_exp(e) 
 	{}
 
 	Expr* get_expr() const {return m_exp;}
@@ -49,7 +62,7 @@ private:
 
 public:
 	Ref_decl(std::string name, Type* t, Expr* e)
-	: Decl(name,t) , m_exp(e)
+	: Decl(name,t,ref_decl) , m_exp(e)
 	{}
 
 	Expr* get_expr() const { return m_exp;}
@@ -60,12 +73,18 @@ class Func_decl : public Decl
 {
 private:
 	Stmt *m_stmt;
-	
+	std::vector<Decl*> param_list;
 
 public:
 	Func_decl(std::string name, Type* t,Stmt* s)
-	:Decl(name,t) , m_stmt(s)
+	:Decl(name,t,func_decl) , m_stmt(s)
 	{}
+
+	void add_param(Decl* d) 
+	{param_list.push_back(d);}
+
+	std::vector<Decl*>   get_params() const
+	{return param_list;}
 
 	Stmt* get_stmt () const { return m_stmt;}
 };
